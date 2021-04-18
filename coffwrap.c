0ef4cb9d4b4e9cfc6ca40671c153a831a90e5353
Jeffrey H. Johnson <trnsz@pobox.com>
2021-04-18 17:40:00 -0400

    coffwrap: Compatibility with ancient C compilers

--- coffwrap.c
+++ coffwrap.c
@@ -3,62 +3,69 @@
 #include <string.h>
 #include "version.h"
 
-int i, cnt;
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (C) 2021 Jeffrey H. Johnson <trnsz@pobox.com>
+ * Copyright (C) 2015 Ondrej Novak
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+int i;
+unsigned int cnt;
 unsigned int size;
 
+#define FOPEN_MODE "rb"
+
 int main(
-  int argc,
-  char **argv
-) {
+  argc,
+  argv
+) 
+  int argc;
+  char **argv;
+{
 
   if (
     argc != 4
 ) {
-    fputs(
-      "Convert binary file to C.\n"
-          /*
-
-             The MIT License (MIT)
-             Copyright (c) 2015 Ondrej Novak
-
-             Permission is hereby granted, free of charge, to any person
-             obtaining a copy of this software and associated documentation
-             files (the "Software"), to deal in the Software without
-             restriction, including without limitation the rights to use, copy,
-             modify, merge, publish, distribute, sublicense, and/or sell copies
-             of the Software, and to permit persons to whom the Software is
-             furnished to do so, subject to the following conditions:
-
-             The above copyright notice and this permission notice shall be
-             included in all copies or substantial portions of the Software.
-
-             THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-             EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-             MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-             NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-             BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-             ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-             CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-             SOFTWARE.
-
-           */
-          "\nUsage:\n"
-          "        coffwrap <file> <identifier> <name>\n\n",
-          stderr
+    fprintf(
+		  stderr,
+          "\r\nUsage:\r\n    %s <file> <identifier> <name>\r\n\n",
+		  argv[0]
     );
     return 1;
   } else {
     FILE *f = strcmp(
       argv[1], "-"
     ) == 0 ? stdin : fopen(
-          argv[1], "rb"
+          argv[1],
+		  FOPEN_MODE
     );
 
     if (
           f == NULL
     ) {
       fprintf(
-            stderr, "Can't open input file '%s'\n",
+            stderr, "\r\nError: could not open input file '%s'\r\n",
                 argv[1]
       );
       return errno;
@@ -70,7 +77,7 @@ int main(
       ) > 13
     ) {
     fprintf(
-      stderr, "Error: filename too long.\n"
+      stderr, "\r\nError: filename too long.\r\n"
     );
       return 1;
     }
@@ -105,10 +112,7 @@ int main(
         size++;
 
         if (
-          i >= 32 && \
-		    i != '"' && \
-            i != '\\' && \
-            i < 128
+          i >= 32 && i != '"' && i != '\\' && i < 128
         ) {
           putchar(
             i
